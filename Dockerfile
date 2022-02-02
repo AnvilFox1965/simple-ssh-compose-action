d945462523e5c20c0f4172dc3bf640049abdddc1
Alexander Aprelikov <aprelikov.do@gmail.com>
2022-02-03 00:02:10 +0300

    repo: fix build

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM alpine:latest
-
 RUN apk --no-cache add openssh-client
-
-COPY docker-entrypoint.sh /docker-entrypoint.sh
-
-ENTRYPOINT ["/docker-entrypoint.sh"]
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,71 +1,50 @@
 #!/bin/sh
 set -eu
 
-execute_ssh() {
-  echo "Execute SSH command: $*"
+ssh_remote_command() {
   ssh -q -t -i "$HOME/.ssh/id_rsa" \
     -o UserKnownHostsFile=/dev/null \
-    -p "$INPUT_REMOTE_DOCKER_PORT" \
-    -o StrictHostKeyChecking=no "$INPUT_REMOTE_DOCKER_HOST" "$*"
+    -p "$DOCKER_PORT" \
+    -o StrictHostKeyChecking=no "$DOCKER_HOST" "$*"
 }
 
-if [ -z "$INPUT_REMOTE_DOCKER_HOST" ]; then
-  echo "Input remote_docker_host is required!"
+if [ -z "$SSH_PUB" ]; then
+  echo "SSH_PUB missing"
   exit 1
 fi
 
-if [ -z "$INPUT_REMOTE_DOCKER_PORT" ]; then
-  INPUT_REMOTE_DOCKER_PORT=22
-fi
-
-if [ -z "$INPUT_SSH_PUBLIC_KEY" ]; then
-  echo "Input ssh_public_key is required!"
+if [ -z "$SSH_PRIV" ]; then
+  echo "SSH_PRIV missing"
   exit 1
 fi
 
-if [ -z "$INPUT_SSH_PRIVATE_KEY" ]; then
-  echo "Input ssh_private_key is required!"
+if [ -z "$DOCKER_HOST" ]; then
+  echo "SSH_PRIV missing"
   exit 1
 fi
 
-if [ -z "$INPUT_SERVICE_NAME" ]; then
-  echo "Input service_name is required!"
+if [ -z "$DOCKER_PORT" ]; then
+  DOCKER_PORT=22
+fi
+
+if [ -z "$COMPOSE_FILE_PATH" ]; then
+  echo "COMPOSE_FILE_PATH missing"
   exit 1
 fi
-
-if [ -z "$INPUT_DEPLOY_PATH" ]; then
-  echo "Input input_deploy_path is required!"
-  exit 1
-fi
-
-if [ -z "$INPUT_ARGS" ]; then
-  echo "Input input_args is required!"
-  exit 1
-fi
-
-if [ -z "$INPUT_PULL_IMAGES_FIRST" ]; then
-  INPUT_PULL_IMAGES_FIRST=false
-fi
-
-if [ -z "$INPUT_STACK_FILE_NAME" ]; then
-  INPUT_STACK_FILE_NAME=docker-compose.yml
-fi
-
-SSH_HOST=${INPUT_REMOTE_DOCKER_HOST#*@}
 
 echo "Registering SSH keys..."
-
+SSH_HOST=${DOCKER_HOST#*@}
 # register the private key with the agent.
 mkdir -p "$HOME/.ssh"
-printf '%s\n' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/id_rsa"
+printf '%s\n' "$SSH_PRIV" > "$HOME/.ssh/id_rsa"
 chmod 600 "$HOME/.ssh/id_rsa"
 eval "$(ssh-agent)"
 ssh-add "$HOME/.ssh/id_rsa"
-echo "Add known hosts"
-printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" > /etc/ssh/ssh_known_hosts
+echo "Add info to known hosts"
+printf '%s %s\n' "$SSH_HOST" "$SSH_PUB" > /etc/ssh/ssh_known_hosts
 
-if [ -n "$INPUT_PULL_IMAGES_FIRST" ] && [ "$INPUT_PULL_IMAGES_FIRST" = 'true' ]; then
-  execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose pull $INPUT_SERVICE_NAME"
-fi
-
-execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose -f $INPUT_STACK_FILE_NAME $INPUT_ARGS $INPUT_SERVICE_NAME 2>&1"
+ssh_remote_command "cd $COMPOSE_FILE_PATH && docker system prune -f"
+ssh_remote_command "cd $COMPOSE_FILE_PATH && docker volume prune -f"
+ssh_remote_command "cd $COMPOSE_FILE_PATH && docker-compose pull"
+ssh_remote_command "cd $COMPOSE_FILE_PATH && docker-compose down -v"
+ssh_remote_command "cd $COMPOSE_FILE_PATH && docker-compose up -d 2>&1"
